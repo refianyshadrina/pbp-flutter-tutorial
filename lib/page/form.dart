66e64f70cfd43236c3478f2888ee0c05b41f2763
refianys1678 <refianys1678@gmail.com>
2022-11-17 10:51:08 +0700

    lab 8

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test/main.dart';
+import 'package:test/page/to_do_page.dart';
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
@@ -200,6 +201,16 @@ class _MyFormPageState extends State<MyFormPage> {
                       });
                     },
                   ),
+                ),
+                ListTile(
+                  title: const Text('To Do'),
+                  onTap: () {
+                    // Route menu ke halaman to do
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ToDoPage()),
+                    );
+                  },
                 ),
                 SwitchListTile(
                   title: const Text('Practice Mode'),
